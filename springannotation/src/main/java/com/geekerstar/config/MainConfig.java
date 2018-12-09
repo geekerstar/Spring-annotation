d2be7480a620652c609ec0fc562615fe68dcb60b
geekerstar <247507792@qq.com>
2018-12-09 09:53:56 +0800

    组件注册-自定义TypeFilter指定过滤规则

--- springannotation/src/main/java/com/geekerstar/config/MainConfig.java
+++ springannotation/src/main/java/com/geekerstar/config/MainConfig.java
@@ -1,6 +1,7 @@
 package com.geekerstar.config;
 
 import com.geekerstar.bean.Person;
+import com.geekerstar.service.BookService;
 import org.springframework.context.annotation.*;
 import org.springframework.stereotype.Controller;
 import org.springframework.stereotype.Service;
@@ -12,12 +13,20 @@ import org.springframework.stereotype.Service;
  * @ComponentScan value:指定要扫描的包
  * excludeFilter = Filter[]，指定扫描的时候按照扫描规则排除那些组件
  * includeFilter = Filter[]，指定扫描的时候只需要包含哪些组件
+ *
+ * FilterType.ANNOTATION 按照注解
+ * FilterType.ASSIGNABLE_TYPE 按照给定的类型
+ * FilterType.ASPECTJ 使用ASPECTJ表达式
+ * FilterType.REGEX 使用正则指定
+ * FilterType.CUSTOM 使用自定义规则
  */
 @Configuration  //告诉Spring这是个配置类
 @ComponentScans(
         value = {
                 @ComponentScan(value = "com.geekerstar", includeFilters = {
-                        @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = {Controller.class})
+//                        @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = {Controller.class}),
+//                        @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE,classes = {BookService.class}),
+                        @ComponentScan.Filter(type = FilterType.CUSTOM,classes = {MyTypeFIlter.class})
                 }, useDefaultFilters = false)
         }
 )

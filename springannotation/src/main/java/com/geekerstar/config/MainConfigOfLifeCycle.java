ef727fd430760ead0f94cd20d66f90e10f8a7e6b
geekerstar <247507792@qq.com>
2018-12-09 12:40:55 +0800

    生命周期-BeanPostProcessor原理

--- springannotation/src/main/java/com/geekerstar/config/MainConfigOfLifeCycle.java
+++ springannotation/src/main/java/com/geekerstar/config/MainConfigOfLifeCycle.java
@@ -27,6 +27,21 @@ import org.springframework.context.annotation.Scope;
  *      单实例：容器关闭的时候
  *      多实例：容器不会管理这个Bean，容器不会调用销毁方法
  *
+ *
+ * 遍历得到容器中所有的BeanPostProcessor；挨个执行beforeInitialization，
+ * 一但返回null，跳出for循环，不会执行后面的BeanPostProcessor.postProcessorsBeforeInitialization
+ *
+ *
+ * BeanPostProcessor原理
+ * populateBean(beanName, mbd, instanceWrapper);给bean进行属性赋值
+ * initializeBean
+ * {
+ * applyBeanPostProcessorsBeforeInitialization(wrappedBean, beanName);
+ * invokeInitMethods(beanName, wrappedBean, mbd);执行自定义初始化
+ * applyBeanPostProcessorsAfterInitialization(wrappedBean, beanName);
+ *}
+ *
+ *
  * 1、指定初始化和销毁方法
  *      配置的方式：通过@Bean指定init-method和destroy-method
  * 2、通过让Bean实现InitialIzingBean(定义初始化逻辑），DisposableBean(定义销毁）

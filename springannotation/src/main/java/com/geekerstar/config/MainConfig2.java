21f0de1a914587f778323a00bbfb867376841ddd
geekerstar <247507792@qq.com>
2018-12-09 10:13:08 +0800

    组件注册-@Lazy-Bean懒加载

--- springannotation/src/main/java/com/geekerstar/config/MainConfig2.java
+++ springannotation/src/main/java/com/geekerstar/config/MainConfig2.java
@@ -3,6 +3,7 @@ package com.geekerstar.config;
 import com.geekerstar.bean.Person;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.context.annotation.Scope;
 
 /**
@@ -20,9 +21,12 @@ public class MainConfig2 {
      * singleton  单实例（默认） IOC启动会调用方法创建对象放到IOC容器中，以后每次获取直接从容器（map.get())中拿
      * request  同义词请求创建一个实例
      * session  同一个session创建一个实例
-     * @return
+     *
+     * 单实例Bean,默认在容器启动的时候创建对象
+     * 懒加载：容器启动不创建对象，第一次使用（获取）Bean创建对象，并初始化，并且以后每次获取不创建，因为是单实例的
      */
-    @Scope("prototype")
+//    @Scope("prototype")
+    @Lazy
     @Bean("person")
     public Person person(){
         return new Person("geekerstar",22);
